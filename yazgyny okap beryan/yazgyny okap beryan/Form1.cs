b589ce1ddb1463650c8f40e630904baf1fe08ecb
xxYGRxx <147963483+xxYGRxx@users.noreply.github.com>
2023-10-15 09:52:49 +0500

    Add files via upload

---
+++ yazgyny okap beryan/yazgyny okap beryan/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SpeechLib;
+
+namespace yazgyny_okap_beryan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SpVoice ses = new SpVoice();
+            ses.Speak(textBox1.Text, SpeechVoiceSpeakFlags.SVSFDefault);
+        }
+    }
+}
